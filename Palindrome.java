0cac0353c979885be705003adf370102aa2281ed
Priyanka51986403 <91080815+Priyanka51986403@users.noreply.github.com>
2021-09-28 15:50:02 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Palindrome {
+	public static void main(String[] args) {
+		int value=0;
+		for(int i=100;i<=999;i++) {
+			for(int j=i;j<=999;j++) {
+				int m=j*i;
+				StringBuilder sb=new StringBuilder(""+m);
+				String b=""+m;
+				sb.reverse();
+				if(b.equals(sb.toString())&&value<m) {
+					value=m;
+				}
+			}
+		}
+		System.out.println(value);
+	}
+
+}
